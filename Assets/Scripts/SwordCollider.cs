7a806fe2182041596066caafe45fc36b405a4110
Jean-Vincent Lamberti <jv.lamberti@live.fr>
2013-12-01 17:50:43 +0100

    Models & Stuff

---
+++ Assets/Scripts/SwordCollider.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnColliderEnter(Collider col)
+    {
+        if ((GameMgr.Instance.Type & GameMgrType.SERVER) != 0)
+            return;
+        if (col.CompareTag("Player"))
+        {
+
+        }
+    }
+}
